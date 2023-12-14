c7faa8b49ec27282190f96fdda08f5e60c829c9f
TonyTenev <tentchotenev@gmail.com>
2023-12-14 10:08:05 -0600

    reRuneer class

---
+++ src/test/java/OpenMRS/runners/ReRunner.java
@@ -0,0 +1,16 @@
+package OpenMRS.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features ="target/uiFailedTests.txt",
+        glue = "/Users/tentchotenev/Desktop/codefish/codefishProjectInternship/src/test/java/OpenMRS/stepDef",
+        dryRun = false,
+//        tags = "@regression",
+        plugin = {"pretty","html:target/uiReport.html","rerun:target/uiFailedTests.txt"}
+)
+public class ReRunner {
+}
